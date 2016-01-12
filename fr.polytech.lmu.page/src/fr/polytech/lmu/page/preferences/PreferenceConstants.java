c86772887ba3008dc97d2173be3d9a20a1bdea9f
Clément Forneris <clement.forneris@gmail.com>
2016-01-12 20:36:10 +0100

    Preferences Page LMU

---
+++ fr.polytech.lmu.page/src/fr/polytech/lmu/page/preferences/PreferenceConstants.java
@@ -0,0 +1,12 @@
+package fr.polytech.lmu.page.preferences;
+
+/**
+ * Constant definitions for plug-in preferences
+ */
+public class PreferenceConstants {
+
+	public static String P_PATH = "pathPreference";
+
+	public static String P_CHOICE = "choicePreference";
+	
+}
